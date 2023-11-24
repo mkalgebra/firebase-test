26bf2f01653fb078fa5e097336bdb8c8e93d9176
Marijo Kis <marijo.kis@q.agency>
2023-11-24 11:33:56 +0100

    chore: test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection, addDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [newData, setNewData] = useState({
+    // Update the data model with two string and two number inputs
+    away_name: "",
+    home_name: "",
+    home_points: 0,
+    away_points: 0,
+  });
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "rez"));
+
+        const newData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setData(newData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewData((prevData) => ({
+      ...prevData,
+      [name]:
+        name === "home_points" || name === "away_points"
+          ? parseInt(value, 10)
+          : value,
+    }));
+  };
+
+  const handleAddData = async () => {
+    try {
+      await addDoc(collection(db, "rez"), newData);
+
+      // Clear the form after successful addition
+      setNewData({
+        away_name: "",
+        home_name: "",
+        home_points: 0,
+        away_points: 0,
+      });
+    } catch (error) {
+      console.error("Error adding data:", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.id}</li>
+        ))}
+      </ul>
+
+      <form>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="home_name"
+            value={newData.home_name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="away_name"
+            value={newData.away_name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Age:
+          <input
+            type="number"
+            name="home_points"
+            value={newData.home_points}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Score:
+          <input
+            type="number"
+            name="away_points"
+            value={newData.away_points}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleAddData}>
+          Add Data
+        </button>
+      </form>
     </div>
   );
 }
